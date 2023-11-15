624051d42a76655f0051336446507fdfb93426c3
Mister KoK <62157776+MisterKoK@users.noreply.github.com>
2023-11-15 20:29:41 +0300

    Add files via upload

---
+++ Aiogram Бот(3.1.1)/bot-1.py
@@ -0,0 +1,31 @@
+
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token="token1233")
+# Диспетчер
+dp = Dispatcher()
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+# Добавлять эмодзи
+@dp.message(Command("dice"))
+async def cmd_dice(message: types.Message):
+    await message.answer_dice(emoji="🎲")
+
+
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
